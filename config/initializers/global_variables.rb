d12323c862516bf34dac08957c5d6787423a115e
Lee <lee@zion-ubuntu.localdomain>
2010-03-16 10:35:33 +0000

    Made rss more compliant

--- config/initializers/global_variables.rb
+++ config/initializers/global_variables.rb
@@ -3,7 +3,7 @@
 DELETE_CONFIRM = "Are you sure you want to delete?\nThis can not be undone."
 SEARCH_LIMIT = 25
 SITE_NAME = 'PirateMyFilm'
-SITE = RAILS_ENV == 'production' ? 'www.piratemyfilm.com' : 'localhost:3000'
+SITE = RAILS_ENV == 'production' ? 'http://www.piratemyfilm.com' : 'localhost:3000'
 
 
 MAILER_TO_ADDRESS = 'info@piratemyfilm.com'
