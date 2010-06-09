552955805c6a9e389c9c29559d8b70df0c218fd4
Lee <lee@zion-ubuntu.localdomain>
2010-06-09 11:10:45 +0100

    Fixed deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -171,7 +171,7 @@ namespace :ferret do
 
   desc "Ferret Index"
   task :index do
-    run "cd #{current_path} && rake ferret_index"
+    run "cd #{current_path} && RAILS_ENV=production rake ferret_index"
   end
 
   desc "Ferret Start"
