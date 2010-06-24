0ed51a04214ccd09611a6c6b6123b8ee7c6c8e55
Lee <lee@zion-ubuntu.localdomain>
2010-06-24 07:11:44 +0100

    Added missing files

---
+++ db/migrate/20100623154832_load_trailer_genre.rb
@@ -0,0 +1,9 @@
+class LoadTrailerGenre < ActiveRecord::Migration
+  def self.up
+    Genre.create(:title => "Trailer")
+  end
+
+  def self.down
+    Genre.find_by_title(:title => "Trailer").destroy!
+  end
+end
