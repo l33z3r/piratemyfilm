e66d529225a5150d3a689373786924624cc42f7f
Lee <lee@zion-ubuntu.localdomain>
2010-08-26 08:06:32 +0100

    added mail host to production config AGAIN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ config.action_controller.perform_caching             = true
 # needed for Avatar::Source::RailsAssetSource
 config.action_controller.asset_host                  = "http://piratemyfilm.piratemyfilm-001.vm.brightbox.net"
 
-config.action_mailer.default_url_options = {:host => "piratemyfilm.com"}
+config.action_mailer.default_url_options = {:host => "www.piratemyfilm.com"}
 
 # Disable delivery errors, bad email addresses will be ignored
 #config.action_mailer.raise_delivery_errors = false
